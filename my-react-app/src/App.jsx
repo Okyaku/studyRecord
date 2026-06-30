db1a2d7971c621017e3fc248b6a971a7bd07beff
Sora Takahashi <ee231027@senshu-u.jp>
2026-07-01 05:34:03 +0900

    third

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,80 +2,50 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [content, setContent] = useState('')
-  const [time, setTime] = useState('')
-  const [recordContent, setrecordContent] = useState([])
+  const [count,setcount] = useState('')
+  const [time,settime] = useState('')
+  const [error,seterror] = useState('')
 
-  const handleRegister = () =>  {
-    if (content === '' || time === '') {
-      alert('学習内容と学習時間を入力してください')
-    return }
-      alert(`学習内容: ${content}\n学習時間: ${time}分`)
-
-  const newRecodeContent = {
-    content: content,
-    time: time
+  const handleClick = () => {
+    if (count ==='',time === '') {
+      seterror('Please enter a valid number')
+      return
+    }
+    seterror('')
+    alert(`:学習内容 ${count}, 時間: ${time}`)
   }
-  setrecordContent([...recordContent, newRecodeContent])
-  setContent('')
-  setTime('')
-  }
-
-
-  const rowstyle = {
-     display: 'flex',
-     justifyContent: 'center',
-     alignItems: 'center', 
-     gap: '10px', 
-     marginBottom: '20px'
+  const rowStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '10px'
   }
   
   return (
     <>
-      {/* display: 'flex' を追加して横並びに */}
-      <div style={rowstyle}>
-        {/* pタグだとデフォルトで上下に余白がつくので、spanタグに変更するとキレイに揃います */}
-        <span>学習内容</span>
-        <input 
-          type="text" 
-          placeholder="学習内容を入力してください" 
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-      </div>
-      
-      <div style={rowstyle}>
-        <span>学習時間</span>
-        <input 
-          type="text" 
-          placeholder="学習時間を入力してください" 
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
-        <span>分</span>
-      </div> 
-      <div style={rowstyle}>
-        <span>入力されている学習内容:</span>
-        <span>{content}</span>
-      </div>
-      <div style={rowstyle}>
-        <span>入力されている学習時間:</span>
-        <span>{time}</span>
-      </div>
-      <div style={rowstyle}>
-        <button onClick={handleRegister}>登録</button>
-      </div>
-      <div style={rowstyle}>
-        <span>登録された学習内容:</span>
-        <ul>
-          {recordContent.map((record, index) => (
-            <li key={index}>
-              学習内容: {record.content}, 学習時間: {record.time}分
-            </li>
-          ))}
-        </ul>
-      </div>
-
+    <div style={{textAlign: 'center'}}>
+      {error}
+    </div>
+    <div style={rowStyle}>
+      <span>学習内容</span>
+      <input
+      placeholder="学習内容を入力してください"
+      value={count}
+      onChange={(e) => setcount(e.target.value)}
+      />
+    </div>
+    <div style={rowStyle}>
+      <span>学習時間</span>
+      <input
+      placeholder="学習時間を入力してください"
+      value={time}
+      onChange={(e) => settime(e.target.value)}
+      />
+    </div>
+    <div style={rowStyle}>
+    <button onClick={handleClick}>登録</button>
+    </div>
     </>
   )
 }
